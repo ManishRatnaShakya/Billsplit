caf5e0fe4221a2cb304bb2ba94fc5d1d1dc62a73
pukarsai <85217415+pukarsai@users.noreply.github.com>
2025-09-07 13:53:20 +0545

    Generic Response Changes

--- BillSplit.Application/Interfaces/IUserService.cs
+++ BillSplit.Application/Interfaces/IUserService.cs
@@ -4,7 +4,7 @@ namespace BillSplit.Application.Interfaces;
 
 public interface IUserService
 {
-    Task<ResponseDto> LoginAsync(LoginDto loginDto);
+    Task<string> LoginAsync(LoginDto loginDto);
     Task SignUpAsync(SignUpDto userDto);
     Task UpdateUser(Guid id, UserUpdateDto userUpdateDto);
 }
